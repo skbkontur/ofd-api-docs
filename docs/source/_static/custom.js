5e6216724ca2ca03cb0f52c51710d7dc77256624
Евгения Горяева <ev.goryaeva@mail.ru>
2022-03-31 18:11:45 +0500

    Переадресация на https://docs-ofd-api.kontur.ru/

--- docs/source/_static/custom.js
+++ docs/source/_static/custom.js
@@ -2,3 +2,4 @@ $(document).ready(function() {
   $("#rtd-search-form").find("[name='q']").attr("placeholder","Поиск..."); // translate placeholder
   $(".wy-breadcrumbs li:first-of-type > a").html("API Контур.ОФД") // fix breadcrubs strart
 });
+window.location.replace('https://docs-ofd-api.kontur.ru/');
